7e418f4c9388babf12d73d22f685e41083d91b31
Raphael Nobre <nobre.roots@gmail.com>
2023-05-10 01:32:54 -0300

    exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,20 +5,28 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  
+  const average = total === 0 ? 0 : (good - bad) / total 
+
+  const positive = total === 0 ? 0 : (good * 100) / total
 
   const handleGoodClick = () => {
     console.log(good + 1, 'you voted good')
     setGood(good + 1)
+    setTotal(total + 1)
   }
 
   const handleNeutralClick = () => {
     console.log(neutral + 1, 'you voted neutral')
     setNeutral(neutral + 1)
+    setTotal(total + 1)
   }
 
   const handleBadClicks = () => {
     console.log(bad + 1, 'you voted bad')
     setBad(bad + 1)
+    setTotal(total + 1)
   }
 
   return (
@@ -31,6 +39,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
